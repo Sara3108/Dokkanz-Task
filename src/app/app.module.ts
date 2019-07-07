3ac2396c91672315caf08616ace9f3e5f20ffd9e
Sara3108 <51201757+Sara3108@users.noreply.github.com>
2019-07-08 00:17:48 +0200

    Add product  modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,23 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
 import { CategoriesComponent } from './Components/categories/categories.component';
 import { ProductsComponent } from './Components/products/products.component';
 import { RouterModule } from '@angular/router';
+import { DefaultComponent } from './Components/default/default.component';
+import { AddProductComponent } from './Components/add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CategoriesComponent,
-    ProductsComponent
+    ProductsComponent,
+    DefaultComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      // {path:'', component:CategoriesComponent},
+      {path:'', component:DefaultComponent},
       {path:'product/:id', component:ProductsComponent}
 
     ])
